2f0740d1a6f3f6bbcd1bd6893091745fb7931322
Burhan <muhammadburhansheikh31@gmail.com>
2021-11-12 14:47:12 +0500

    gof commit 1.6

---
+++ src/BL/GameLogic/cell.java
@@ -0,0 +1,55 @@
+package GameLogic;
+public class cell {
+   
+    int x,y;
+   boolean Alive;
+   int neighbourCount;
+
+   public cell()
+   {
+       x=y=neighbourCount=-1;
+       Alive=false;
+   }
+   public cell(int X,int Y,boolean alive)
+   {
+      x=X;
+      y=Y;
+      Alive=alive;   
+   }
+    public void setPosition(int X,int Y)
+    {
+        x=X;
+        y=Y;
+
+    }
+   public boolean isAlive()
+   {
+        return Alive;
+   }
+   public void setStatus(boolean val)
+   {
+        Alive=val;
+
+   }
+   public void setNeighbours(int x)
+   {
+neighbourCount=x;       
+   }
+   public int getNeighboursCount()
+   {
+       return neighbourCount;
+   }
+   public void printCell()
+   {
+if(Alive)
+{
+    System.out.print("o");
+}
+else
+{
+    System.out.print(".");
+}
+   }
+
+
+}
