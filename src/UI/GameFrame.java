8b335b975a98b5e3c6cfc5b172cb642e9e81f549
Musa Malik <muhammadmusamalik2@gmail.com>
2021-11-14 20:15:10 +0500

    gof commit 2.9

--- src/UI/GameFrame.java
+++ src/UI/GameFrame.java
@@ -1,77 +1,121 @@
 package UI;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GameFrame extends JFrame implements ChangeListener{
+public class GameFrame extends JFrame implements ChangeListener, ActionListener
+{
+    MainButton start,reset,next;
+    int counter,delay;
+    JLabel label;
 
-
-    //JButton start,reset,stop,clear;
-    SliderFrame slide;
-    ButtonFrame btns;
     int rows;
     int columns;
-//    JSlider slider;
-//    JSlider speeder;
+    JSlider slider,speeder;
     JPanel GridPanel;
-    //int GridArray[][];
     Cell[][] c;
 
+    Timer timer = new Timer(500, new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            counter++;
+            label.setText(Integer.toString(counter));
+        }
+    });
+
     public GameFrame(){
-//        start = new JButton("START");
-//        reset = new JButton("RESET");
-//        stop = new JButton("STOP");
-//        clear = new JButton("CLEAR");
-        btns = new ButtonFrame();
-        slide = new SliderFrame();
-//        slider = new JSlider(0,100,50);
-//        speeder = new JSlider(0,100,50);
+        counter = 0;
+        delay = 0;
+        label = new JLabel("");
+        label.setFont(new Font("Consolas",Font.PLAIN,14));
+
+        start = new MainButton("START");
+        start.btn.addActionListener(this);
+
+        next = new MainButton("NEXT");
+        next.btn.addActionListener(this);
+
+        reset = new MainButton("RESET");
+        reset.btn.addActionListener(this);
+
+        label.setText(Integer.toString(this.counter));
+
+        slider = new JSlider(0,50,25);
+        speeder = new JSlider(0,50,25);
+
+        speeder.setPreferredSize(new Dimension(100,50));
+        speeder.setBackground(Color.yellow);
+
+        speeder.setPaintTrack(true);
+        speeder.setMajorTickSpacing(10);
+        speeder.addChangeListener(this);
+
+        slider.setPreferredSize(new Dimension(100,50));
+        slider.setBackground(Color.pink);
+
+        slider.setPaintTrack(true);
+        slider.setMajorTickSpacing(10);
+        slider.addChangeListener(this);
+
+        slider.addChangeListener(this);
+        speeder.addChangeListener(this);
+
         //this.rows = Integer.parseInt(r);
         this.rows = 20;
         //this.columns = Integer.parseInt(c);
         this.columns = 40;
 
-        c = new Cell [this.rows][];
-
-        for (int i = 0; i < this.columns; i++) {
-            c = new Cell [i][this.columns];
-        }
-
-        slide.setColumns(this.columns);
-        slide.setRows(this.rows);
         this.UI_FRAME();
     }
 
     public GameFrame(String r, String co){
 
-//        start = new JButton("START");
-//        reset = new JButton("RESET");
-//        stop = new JButton("STOP");
-//        clear = new JButton("CLEAR");
-        btns = new ButtonFrame();
-        slide = new SliderFrame();
-//        slider = new JSlider(0,100,50);
-//        speeder = new JSlider(0,100,50);
+        counter = 0;
+        delay = 0;
+        label = new JLabel("");
+
+        start = new MainButton("START");
+        start.btn.addActionListener(this);
+
+        next = new MainButton("NEXT");
+        next.btn.addActionListener(this);
+
+        reset = new MainButton("RESET");
+        reset.btn.addActionListener(this);
+
+        label.setText(Integer.toString(this.counter));
+
+        slider = new JSlider(0,50,25);
+        speeder = new JSlider(0,50,25);
+
+        speeder.setPreferredSize(new Dimension(100,50));
+        speeder.setBackground(Color.yellow);
+
+        speeder.setPaintTrack(true);
+        speeder.setMajorTickSpacing(10);
+        speeder.addChangeListener(this);
+
+        slider.setPreferredSize(new Dimension(100,50));
+        slider.setBackground(Color.pink);
+        //slider.setPaintTicks(true);
+        //slider.setMinorTickSpacing(2);
+
+        slider.setPaintTrack(true);
+        slider.setMajorTickSpacing(10);
+        slider.addChangeListener(this);
+
+        slider.addChangeListener(this);
+        speeder.addChangeListener(this);
 
         this.rows = Integer.parseInt(r);
         //this.rows = 100;
         this.columns = Integer.parseInt(co);
         //this.columns = 100;
 
-        c = new Cell [this.rows][];
-
-        for (int i = 0; i < this.columns; i++) {
-            c[i] = new Cell [this.columns];
-        }
-
-        slide.setColumns(this.columns);
-        slide.setRows(this.rows);
         this.UI_FRAME();
     }
 
@@ -90,7 +134,6 @@ public class GameFrame extends JFrame implements ChangeListener{
         JPanel Jrpanel2 = new JPanel();
         JPanel Jrpanel3 = new JPanel();
         JPanel Jrpanel4 = new JPanel();
-        //JPanel Jrpanel5 = new JPanel();
         GridPanel = new JPanel();
 
         Jrpanel1.setPreferredSize(new Dimension(13,13));
@@ -99,34 +142,19 @@ public class GameFrame extends JFrame implements ChangeListener{
         Jrpanel4.setPreferredSize(new Dimension(13,13));
         GridPanel.setPreferredSize(new Dimension(100,100));
 
-        //Jrpanel5.setLayout(new GridLayout(this.rows,this.columns));
-
         Jrpanel1.setBackground(Color.darkGray);
         Jrpanel2.setBackground(Color.darkGray);
         Jrpanel3.setBackground(Color.darkGray);
         Jrpanel4.setBackground(Color.darkGray);
-        //Jrpanel5.setBackground(Color.yellow);
 
-        panel3.setLayout(new BorderLayout());
-//new GridLayout(this.rows,this.columns)
         panel1.setBackground(Color.red);
         panel2.setBackground(Color.pink);
+        panel3.setLayout(new BorderLayout());
         panel3.setBackground(Color.white);
 
         panel1.setPreferredSize(new Dimension(100,100));
         panel2.setPreferredSize(new Dimension(100,100));
         panel3.setPreferredSize(new Dimension(100,100));
-
-//        JLabel label = new JLabel();
-//        ImageIcon top = new ImageIcon("top.PNG");
-//        label.setIcon(top);
-//        panel1.setLayout(null);
-//        panel1.add(label);
-
-//        BufferedImage myPicture = ImageIO.read("top.PNG"));
-//        JLabel picLabel = new JLabel(new ImageIcon(myPicture));
-//        //add(picLabel);
-//        panel1.add(picLabel);
 
         this.add(panel1,BorderLayout.NORTH);
         this.add(panel2,BorderLayout.SOUTH);
@@ -139,60 +167,51 @@ public class GameFrame extends JFrame implements ChangeListener{
         panel3.add(GridPanel,BorderLayout.CENTER);
 
         this.UI_GRID();
-//        for (int i = 0; i < this.rows; i++) {
-//            for (int j = 0; j < this.columns; j++) {
-//                JButton btn = new JButton("");
-//                btn.setBackground(Color.lightGray);
-//                btn.setBorder(BorderFactory.createEtchedBorder());
-//                Jrpanel5.add(btn);
-//            }
-//        }
 
-//        speeder.setPreferredSize(new Dimension(100,50));
-//        speeder.setBackground(Color.yellow);
-//        //slider.setPaintTicks(true);
-//        //slider.setMinorTickSpacing(2);
-//
-//        speeder.setPaintTrack(true);
-//        speeder.setMajorTickSpacing(10);
-//        speeder.addChangeListener(this);
-//        panel2.add(speeder);
-//
-//        slider.setPreferredSize(new Dimension(100,50));
-//        slider.setBackground(Color.pink);
-//        //slider.setPaintTicks(true);
-//        //slider.setMinorTickSpacing(2);
-//
-//        slider.setPaintTrack(true);
-//        slider.setMajorTickSpacing(10);
-//        slider.addChangeListener(this);
-//        //slider.setPaintLabels(true);
-        slide.speeder.addChangeListener(this);
-        slide.slider.addChangeListener(this);
-        panel2.add(slide.speeder);
-        panel2.add(slide.slider);
-//        panel2.add(start);
-//        panel2.add(reset);
-//        panel2.add(clear);
-//        panel2.add(stop);
-        panel2.add(btns.start.btn);
-        panel2.add(btns.reset.btn);
-        //panel2.add(btns.stop.btn);
-        panel2.add(btns.next.btn);
-        panel2.add(btns.label);
+        speeder.addChangeListener(this);
+        slider.addChangeListener(this);
+
+        panel2.add(speeder);
+        panel2.add(slider);
+
+        panel2.add(start.btn);
+        panel2.add(reset.btn);
+        panel2.add(next.btn);
+        panel2.add(label);
 
         this.setVisible(true);
-        //this.PrintCellLocation();
     }
 
     public void UI_GRID(){
         GridPanel.setLayout(new GridLayout(this.rows,this.columns));
+
+        c = new Cell [this.rows][];
+
+        for (int i = 0; i < this.columns; i++) {
+            c = new Cell [i][this.columns];
+        }
         for (int i = 0; i < this.rows; i++) {
             for (int j = 0; j < this.columns; j++) {
-//                JButton btn = new JButton("");
-//                btn.setBackground(Color.lightGray);
-//                btn.setBorder(BorderFactory.createEtchedBorder());
-//                System.out.println(btn.getLocation());
+                c[i][j] = new Cell(i,j);
+                GridPanel.add(c[i][j].getBtn());
+            }
+        }
+    }
+
+    public void UpdateUI_Grid(int r, int co){
+        this.rows = r;
+        this.columns = co;
+        GridPanel.setLayout(new GridLayout(this.rows,this.columns));
+
+        c = new Cell [this.rows][];
+
+        for (int i = 0; i < this.columns; i++) {
+            c = new Cell [i][this.columns];
+        }
+//        GridPanel.removeAll();
+//        GridPanel.setLayout(new GridLayout(2,4));
+        for (int i = 0; i < this.rows; i++) {
+            for (int j = 0; j < this.columns; j++) {
                 c[i][j] = new Cell(i,j);
                 GridPanel.add(c[i][j].getBtn());
             }
@@ -208,20 +227,63 @@ public class GameFrame extends JFrame implements ChangeListener{
         }
     }
 
+    public void StartGame(){
+        timer.start();
+    }
+
+    public void StopGame(){
+        timer.stop();
+    }
+
+    public void NextGame(){
+        counter++;
+        label.setText(Integer.toString(counter));
+    }
+
+    public void ResetGame(){
+        timer.stop();
+        this.counter = 0;
+        label.setText(Integer.toString(counter));
+        start.click = false;
+        start.btn.setText("START");
+    }
+
     @Override
     public void stateChanged(ChangeEvent e) {
-        if(e.getSource() == slide.speeder){
-            System.out.println(slide.speeder.getValue());
-            if(slide.speeder.getValue() > 25){
-                int check = 50 - slide.speeder.getValue();
-                btns.delay = check * 10 ;
-                btns.timer.setDelay(btns.delay);
+        if(e.getSource() == speeder){
+            System.out.println(speeder.getValue());
+            if(speeder.getValue() > 25){
+                int check = 50 - speeder.getValue();
+                this.delay = check * 10 ;
+                this.timer.setDelay(this.delay);
             }
-            else if(slide.speeder.getValue() < 25){
-                int check = 50 - slide.speeder.getValue();
-                btns.delay = check * 20 ;
-                btns.timer.setDelay(btns.delay);
+            else if(speeder.getValue() < 25){
+                int check = 50 - speeder.getValue();
+                this.delay = check * 20 ;
+                this.timer.setDelay(this.delay);
             }
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==start.btn){
+            if(!start.click){
+                start.click = true;
+                start.btn.setText("STOP");
+                this.StartGame();
+            }
+            else{
+                start.click = false;
+                start.btn.setText("START");
+                this.StopGame();
+            }
+        }
+        if(e.getSource()==next.btn){
+            this.NextGame();
+        }
+        if(e.getSource()==reset.btn){
+            this.ResetGame();
         }
     }
 }
