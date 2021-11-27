18759cfc60b170171af36e503b127bdf581235c4
Burhan <muhammadburhansheikh31@gmail.com>
2021-11-27 10:36:23 +0500

    gof commit 4.6

--- src/UI/Console.java
+++ src/UI/Console.java
@@ -2,6 +2,8 @@ package UI;
 
 import BL.GameLogic.GUI_implementation;
 import BL.GameLogic.Grid;
+import BL.GameLogic.Grid_Controller;
+import BL.GameLogic.Rules;
 
 import java.util.Scanner;
 
@@ -20,6 +22,7 @@ public class Console {
         speederCount = 0;
         counter = 0;
         delay = 0;
+        arr=new int[60][80];
         for (int i = 0; i < 60; i++) {
             for (int j = 0; j < 80; j++) {
 
@@ -27,7 +30,7 @@ public class Console {
 
             }
         }
-        arr=new int[60][80];
+
         implementation= new GUI_implementation();
         implementation= obj;
         rows=20;
@@ -54,5 +57,202 @@ public class Console {
         }
        arr[x][y]=1;
 
+    }
+    public void start()
+    {
+        if(start==true)
+        {
+            return;
+        }
+       Thread GameLoop=new Thread(new Runnable() {
+           @Override
+           public void run() {
+
+               start=true;
+               for (;; ) {
+                if(stop==true)
+                {
+                    stop=false;
+                    break;
+                }
+
+                   int arr2[][] = new int[60][80];
+                   arr2 = implementation.next(arr);
+                   for (int i = 0; i < 60; i++) {
+                       for (int j = 0; j < 80; j++) {
+
+                           arr[i][j] = arr2[i][j];
+
+                       }
+
+                   }
+                   try {
+                       Thread.sleep(speederCount*500);
+                   }catch (Exception e) {
+
+                       // catching the exception
+                       System.out.println(e);
+                   }
+               counter++;
+               }
+           }
+       });
+start=false;
+    }
+    public void next()
+    {
+        if(next==true)
+        {
+            return;
+        }
+
+        Thread GameLoop=new Thread(new Runnable() {
+            @Override
+            public void run() {
+next=true;
+
+
+                    int arr2[][] = new int[60][80];
+                    arr2 = implementation.next(arr);
+                    for (int i = 0; i < 60; i++) {
+                        for (int j = 0; j < 80; j++) {
+
+                            arr[i][j] = arr2[i][j];
+
+                        }
+
+                    }
+                    try {
+                        Thread.sleep(speederCount*500);
+                    }catch (Exception e) {
+
+                        // catching the exception
+                        System.out.println(e);
+                    }
+                    counter++;
+                }
+
+        });
+next=false;
+    }
+    public void stop()
+    {
+        stop=true;
+    }
+    public void reset()
+    {
+        start=reset=next=stop=false;
+        sliderCount = 0;
+        speederCount = 0;
+        counter = 0;
+        delay = 0;
+        for (int i = 0; i < 60; i++) {
+            for (int j = 0; j < 80; j++) {
+
+                arr[i][j] = 0;
+
+            }
+        }
+        rows=20;
+        columns=40;
+    }
+    public void SpeedControl()
+    {
+        Scanner sc= new Scanner(System.in); //System.in is a standard input stream.
+
+        System.out.print("Enter Delay (max 10) : ");
+
+        int x= sc.nextInt();
+
+        speederCount=x;
+    }
+    public void zoomControl()
+    {
+        Scanner sc= new Scanner(System.in); //System.in is a standard input stream.
+
+        System.out.print("Enter no of rows to display (max 60) : ");
+        int x= sc.nextInt();
+        if(x>=60)
+        {
+            System.out.println("max row index is : 60");
+            return;
+
+        }
+
+        System.out.print("Enter no of Columns to display (max 80) : ");
+        int y=sc.nextInt();
+        if(y>=80)
+        {
+            System.out.println("max col index is : 80");
+            return;
+
+        }
+        rows=x;
+        columns=y;
+    }
+
+    public void printGrid()
+    {
+        for(int i=0;i<rows;i++)
+        {
+            for (int j = 0; j < columns; j++) {
+                if(arr[i][j]==0)
+                {
+                    System.out.print("* ");
+                }
+                else if(arr[i][j]==1)
+                {
+                    System.out.print("o ");
+                }
+
+            }
+            System.out.print('\n');
+        }
+        System.out.print("0. Select Cell    1. Start    2. Stop      3.Next     4. Reset     5. Delay    6. Zoom   7. Quit");
+    }
+
+    public void GO() {
+
+        while(1)
+        {
+            printGrid();
+            Scanner sc= new Scanner(System.in); //System.in is a standard input stream.
+
+            System.out.print("Chose : ");
+
+            int x= sc.nextInt();
+            if(x==0)
+            {
+                selectCell();
+            }
+            else if(x==1)
+            {
+                start();
+            }
+            else if(x==2)
+            {
+                stop();
+            }
+            else if(x==3)
+            {
+                next();
+            }
+            else if(x==4)
+            {
+               reset();
+            }
+            else if(x==5)
+            {
+                SpeedControl();
+            }
+            else if(x==6)
+            {
+                zoomControl();
+            }
+            else if(x==7)
+            {
+                break;
+            }
+        }
     }
 }
